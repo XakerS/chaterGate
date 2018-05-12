5ea38ebcd17f3fd1ee0f3f3dd586dbda3b02df60
Aleks Merser <enderman.com290@gmail.com>
2018-05-12 17:31:12 +0400

    Обновлено до версии 2
    Переписано получение

--- chatergate.py
+++ chatergate.py
@@ -1,28 +1,38 @@
 import vk
 import time
 import datetime
-
-#timestamp = 1339521878.04
-
-
-session=vk.Session(access_token="ТОКЕН СЮДА!")
+ 
+session = vk.Session(access_token="21f9450c821b598e48415877aff99a22e71264fc18e421ef1a319e0e5690bc99a64a87a5600dd76c8f23d")
 vk_api = vk.API(session, v='5.62')
-print("Версия v1 "*5)
-ass=vk_api.messages.getDialogs(count=10)
+print("Версия v2 "*5)
+ass = vk_api.messages.getDialogs(count=10)
 print("Список бесед:")
 for msg in ass['items']:
-           if 'chat_id' in msg['message']:
-               print(msg['message']['title'],"|чат-айди »",msg['message']['chat_id'],"«")
+    if 'chat_id' in msg['message']:
+        print(msg['message']['title'], "|чат-айди »",
+              msg['message']['chat_id'], "«")
 print("====chaterGate====")
-targetchat=int(input("Введите id чата для анализа: "))
-targetchat=vk_api.messages.getChat(chat_id=targetchat)
-targetuser=targetchat["users"]
-print(targetuser)
-i=len(targetuser)
-for o in range(len(targetuser)):
- #print(vk_api.users.get(user_ods=o,fields="sex"))
- print("Профиль: ",vk_api.users.get(user_ids=targetuser[o])[0]['first_name'],vk_api.users.get(user_ids=targetuser[o])[0]['last_name'],"Пол:",vk_api.users.get(user_ids=targetuser[o],fields="sex")[0]["sex"],"Дата рождения:",vk_api.users.get(user_id=targetuser[o],fields="bdate")[0]["bdate"])
- timestamp=vk_api.users.get(user_id=targetuser[o],fields="last_seen")[0]["last_seen"]["time"]
- value = datetime.datetime.fromtimestamp(timestamp)
- print("Последний визит:",value.strftime('%Y-%m-%d %H:%M:%S'))
- time.sleep(2)
+#while True:
+##try:
+targetchat = int(input("Введите id чата для анализа: "))
+targetchat = vk_api.messages.getChat(chat_id=targetchat)
+# except Exception as e:
+#      print("Нет такого чат-айди введите верный!!!")
+#      continue
+targetuser = targetchat["users"]
+i = len(targetuser) 
+cnt = 0
+step = 5
+while cnt < i:
+    tmpusers = targetuser[cnt:cnt + step]
+    response = vk_api.users.get(user_ids=tmpusers, fields="first_name,last_name,sex,bdate,last_seen")
+    for user in response:
+        try:
+                print("»Профиль: {} {}, пол: {}, дата рождения: {}".format(user["first_name"], user["last_name"], user["sex"], user["bdate"]))
+                tstamp = user["last_seen"]["time"]
+                value = datetime.datetime.fromtimestamp(tstamp)
+                print("»Последний визит:", value.strftime('%Y-%m-%d %H:%M:%S'))
+        except Exception as e:
+                print("Возникла ошибка у",vk_api.users.get(user_ids=targetuser)[0]["first_name"],vk_api.users.get(user_ids=targetuser)[0]["last_name"],"невозможно получить: {}".format(e))
+    cnt += step
+    time.sleep(0.5)
